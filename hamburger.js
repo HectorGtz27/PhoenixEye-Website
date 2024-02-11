8ca2b6f6001c268e4e560e22ab5dfc7e10366c85
Hector Alan Gutierrez Galvez <114173304+HectorGtz27@users.noreply.github.com>
2024-02-10 17:03:18 -0700

    Add files via upload

---
+++ hamburger.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var menu = document.querySelector('.middle-section');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
+}
